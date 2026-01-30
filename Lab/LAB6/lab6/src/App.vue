fcc20509ba6b87f6d9687bfe2fdf5d0a07f671da
Phong Huynh <phonghuynh2006@gmail.com>
2026-01-30 16:42:53 +0700

    phonghuynh lab6

--- Lab/LAB6/lab6/src/App.vue
+++ Lab/LAB6/lab6/src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
 import Bai1 from './components/bai1.vue';
+import Bai2 from './components/bai2.vue';
+import Bai3 from './components/bai3.vue';
+import bai4 from './components/bai4.vue';
 </script>
 
 <template>
 
     <Bai1 />
 
+    <Bai2 />
+    <Bai3 />
+    <bai4 />
 </template>
 
 <style scoped>
---
+++ Lab/LAB6/lab6/src/components/bai3.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container mt-5">
+    <h2 class="text-center">Kiến thức sức khỏe cộng đồng</h2>
+
+    <div class="row">
+        <div class="col-sm-4 mb-4" v-for="(item, index) in items" :key="index">
+            <div class="card">
+                <img :src="item.image" class="img-fluid" alt="Hình ảnh">
+                <div class="card-body">
+                    <h5 class="card-title">{{ item.title }}</h5>
+                    <p class="card-text">{{ item.content }}</p>
+                    <button class="btn btn-info">Xem chi tiết</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</template>
+<script setup>
+import { ref } from 'vue';
+
+const items = ref([
+  {
+    title: "Vệ sinh cá nhân",
+    content: "Làm sạch cơ thể hàng ngày giúp ngăn ngừa bệnh tật.",
+    image: "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2024/11/tai-hinh-nen-dep-mien-phi.jpg"
+  },
+  {
+    title: "Chế độ ăn uống",
+    content: "Ăn uống điều độ, đủ chất giúp cơ thể khỏe mạnh.",
+    image: "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2024/11/tai-hinh-nen-dep-mien-phi.jpg"
+  },
+  {
+    title: "Tập thể dục đều đặn",
+    content: "Tập luyện giúp tăng cường sức khỏe và tinh thần.",
+    image: "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2024/11/tai-hinh-nen-dep-mien-phi.jpg"
+  }
+]);
+</script>
